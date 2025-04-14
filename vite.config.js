a2aa07fe3fdf281dd3a931174f7953d655750d27
khushibawane <bawanekhushi51@gmail.com>
2025-04-15 00:52:54 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: 'tic-tac-toe',  // 👈 very important
   plugins: [react()],
-  base: '/tic-tac-toe-game/', // Replace with your actual repo name
 });
