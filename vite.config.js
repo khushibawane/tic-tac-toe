cf2f83c15a897a383d9e74f4ea874921ffbe8b0b
Shine8741 <amballauday@gmail.com>
2025-04-14 19:49:32 +0530

    Initial commit of React project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
   base: '/tic-tac-toe-game/', // Replace with your actual repo name
-}
-
+});
